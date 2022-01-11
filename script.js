62263eb1e36ce43e2d604dfb52eb74fea5a21767
OEM Configuration (temporary user) <Victor Tavares>
2022-01-11 18:14:08 -0300

    correções até requisito 3 completas

---
+++ script.js
@@ -0,0 +1,11 @@
+let email = document.querySelector('#email');
+let senha = document.querySelector('#senha');
+const btnEntrar = document.querySelector('.btnEntrar');
+function dispararAlert (){
+    if (email.value === "tryber@teste.com" || senha.value === "123456" ){
+        alert('Olá, Tryber!');
+    } else {
+        alert('Email ou senha inválidos.')
+    }
+}
+btnEntrar.addEventListener('click', dispararAlert);
