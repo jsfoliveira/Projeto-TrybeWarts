7d24a8d4e36e9311bbc89d33def431d20b92a7c2
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-01-12 11:59:52 -0300

    dos obrigatorios, falta finalizar o 15 e 18

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const email = document.querySelector('#email');
 const senha = document.querySelector('#senha');
 const btnEntrar = document.querySelector('.btnEntrar');
+const btnEnviar = document.querySelector('#submit-btn');
+const checkbox = document.querySelector('#agreement');
+
 function dispararAlert() {
     if (email.value === 'tryber@teste.com' || senha.value === '123456') {
     alert('Olá, Tryber!');
@@ -9,3 +12,15 @@ function dispararAlert() {
     }
 }
 btnEntrar.addEventListener('click', dispararAlert);
+
+// Usei as fontes:
+// https://www-w3schools-com.translate.goog/jsref/prop_radio_checked.asp?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt-BR&_x_tr_pto=sc
+// https://www-w3schools-com.translate.goog/tags/att_button_disabled.asp?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt-BR&_x_tr_pto=sc
+function habilitarBtn (){
+    if(checkbox.checked){
+        btnEnviar.disabled = false;
+    } else {
+        btnEnviar.disabled = true;
+    }
+}
+btnEnviar.addEventListener('click', habilitarBtn);
