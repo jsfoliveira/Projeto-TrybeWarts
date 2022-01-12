06d9476086b167d4d9e0a5df04ba78b9289a89e5
OEM Configuration (temporary user) <Victor Tavares>
2022-01-11 21:34:39 -0300

    req 12 com item à resolver

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-let email = document.querySelector('#email');
-let senha = document.querySelector('#senha');
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
 const btnEntrar = document.querySelector('.btnEntrar');
-function dispararAlert (){
-    if (email.value === "tryber@teste.com" || senha.value === "123456" ){
-        alert('Olá, Tryber!');
+function dispararAlert() {
+    if (email.value === 'tryber@teste.com' || senha.value === '123456') {
+    alert('Olá, Tryber!');
     } else {
-        alert('Email ou senha inválidos.')
+        alert('Email ou senha inválidos.');
     }
 }
 btnEntrar.addEventListener('click', dispararAlert);
-
-let header = document
