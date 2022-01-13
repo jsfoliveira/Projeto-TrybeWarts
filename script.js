b7070a59139c79cc3d87d3a886cad24b5bfedff2
OEM Configuration (temporary user) <Victor Tavares>
2022-01-12 21:40:57 -0300

    finalizando o projeto req15 ok

--- script.js
+++ script.js
@@ -5,22 +5,21 @@ const btnEnviar = document.querySelector('#submit-btn');
 const checkbox = document.querySelector('#agreement');
 
 function dispararAlert() {
-    if (email.value === 'tryber@teste.com' || senha.value === '123456') {
+  if (email.value === 'tryber@teste.com' || senha.value === '123456') {
     alert('Olá, Tryber!');
-    } else {
-        alert('Email ou senha inválidos.');
-    }
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 }
 btnEntrar.addEventListener('click', dispararAlert);
-
 // Usei as fontes:
 // https://www-w3schools-com.translate.goog/jsref/prop_radio_checked.asp?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt-BR&_x_tr_pto=sc
 // https://www-w3schools-com.translate.goog/tags/att_button_disabled.asp?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt-BR&_x_tr_pto=sc
-function habilitarBtn (){
-    if(checkbox.checked){
-        btnEnviar.disabled = false;
-    } else {
-        btnEnviar.disabled = true;
-    }
+function habilitarBtn() {
+  if (checkbox.checked) {
+    btnEnviar.disabled = false;
+  } else {
+    btnEnviar.disabled = true;
+  }
 }
 btnEnviar.addEventListener('click', habilitarBtn);
